6bfb35e4abbad0600fcc2ce7e186acbad30aab20
sneha gupta <snehagupta98>
2023-02-08 12:31:57 +0530

    loop#2

---
+++ for.py
@@ -0,0 +1,18 @@
+for i in range(1,10):
+    print("don't cry")
+for i in range(100):
+    print(i , end=' ')
+for i in range(1,101,5):
+    print(i , end=' ')
+print(' ')
+print('ODD NUMBERS')
+#wap to print all odd numbers from 1 to 100
+for i in range(1,100,2) :
+    print( i ,end=' ')
+name= " the way of the king "
+for char in name:
+    print(char)
+movies=['kgf','dhamal','chupke chupke','3 idiots','nadiya par','kuch kuch hota hai']
+for movie in movies:
+    print(f'{movie} | {len(movie)}')
+    
